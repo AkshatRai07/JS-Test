50de4441fc0033d380bc0c8748e313db2e7f316c
AkshatRai07 <sanamrai2007@gmail.com>
2024-11-03 14:18:23 +0000

    datatypes

---
+++ 01_Basics/1_variable.js
@@ -0,0 +1,10 @@
+console.log("Akshat")
+const a = 1
+let b = 2
+var c = 3
+d = 4
+
+/*
+prefer not to use var due to scope issue
+*/
+console.table([a, b, c, d])
---
+++ 01_Basics/2_datatype.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version (auto me new hai)
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3 +
+    3) // code readibility should be high
+
+let name = "akshat"
+let age = 17
+let isLoggedIn = true
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean t or f
+//null => standalone value
+//undefined => val not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
