165cb374b2b72fdff48e55af6ece896fdff45288
AkshatRai07 <sanamrai2007@gmail.com>
2024-11-07 03:42:22 +0000

    datatype summary

---
+++ 01_Basics/4_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+
+// console.log("2" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// equality and comparison operators work differently
+// equality makes direct comparison while comparison converts null to number 0
+// undefined with 0 and any operator always gives false
+// === , equals and same datatype (strict check)
