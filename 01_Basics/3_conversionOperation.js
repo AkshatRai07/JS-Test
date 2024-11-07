81ffa93a584dceb8148588af4ca859839cce54d3
AkshatRai07 <sanamrai2007@gmail.com>
2024-11-07 03:13:21 +0000

    Conversion 2

--- 01_Basics/3_conversionOperation.js
+++ 01_Basics/3_conversionOperation.js
@@ -1,38 +1,49 @@
 let score = "33"
 
-//console.log(typeof score)
-//console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
 
 score = "33abc"
 
 valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
 
 score = null
 
 valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
 
 score = true
 
 valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)  //false --> 0
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)  //false --> 0
 
 let val = NaN
 let boolVal = Boolean(val)
 
-console.log(typeof(boolVal))
-console.log(boolVal)
+// console.log(typeof(boolVal))
+// console.log(boolVal)
 
 // nan, empty string, 0, null are false in boolean
+
+// console.log(2/3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log("1" + 2 + "2")
+// console.log(1 + 2 + "2")
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
