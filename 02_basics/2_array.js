b8042da22fb7a73aeae382af84c44b5dfff92611
AkshatRai07 <sanamrai2007@gmail.com>
2024-11-18 14:29:10 +0000

    Array mk2

--- 02_basics/2_array.js
+++ 02_basics/2_array.js
@@ -16,3 +16,18 @@ console.log(all_new_heros);
 // push does the operation in the same array
 // concat returns a new array
 
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
